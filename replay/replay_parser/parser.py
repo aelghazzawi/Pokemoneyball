d16b8f67799f86ffbf4d9860b095150113ac6eaa
Adam Elghazzawi <adamelghazzawi@yahoo.com>
2019-05-18 19:29:55 -0400

    Added parse_teams method in the parser

--- replay/replay_parser/parser.py
+++ replay/replay_parser/parser.py
@@ -12,3 +12,17 @@ class Parser:
 			players[split_line[2]] = split_line[3]
 		return players
 
+	def parse_teams(self):
+		""" Returns dict of player num -> team. Team represented by list of 6 mons. """
+		pokemon_lines = [line for line in self.text if line.startswith("|poke")]
+		teams = {}
+		for line in pokemon_lines:
+			split_line = line.split("|")
+			player_number = split_line[2]
+			pokemon = split_line[3].split(",", 1)[0]
+			if player_number in teams.keys():
+				teams[player_number].append(pokemon)
+			else:
+				teams[player_number] = [pokemon]
+		return teams
+
--- test_parser.py
+++ test_parser.py
@@ -7,9 +7,16 @@ class ParserTest(TestCase):
     @classmethod
     def setUpClass(cls):
         super(ParserTest, cls).setUpClass()
-        cls.parser = Parser(open(Path.cwd() / "replay" / "replay_parser" / "sample_replay.txt", "r").readlines())
+        cls.parser = Parser(open(Path.cwd() / "sample_replay.txt", "r").readlines())
 
     def test_parse_players(self):
         players = self.parser.parse_players()
         self.assertEqual(players["p1"], "Adaam")
         self.assertEqual(players["p2"], "Ban Manaphy")
+
+    def test_parse_teams(self):
+        teams = self.parser.parse_teams()
+        self.assertEqual(sorted(teams["p1"]), ["Houndoom", "Latias", "Nidoking", "Scizor", "Terrakion", "Togekiss"])
+        self.assertEqual(sorted(teams["p2"]), ["Aerodactyl", "Cobalion", "Gengar", "Krookodile", "Sylveon", "Tsareena"])
+
+
